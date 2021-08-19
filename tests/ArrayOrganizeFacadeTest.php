6dfa015d4882b6e89a0812ef87a76dd428603527
Simon Micheneau <contact@simon-micheneau.fr>
2021-08-19 15:58:05 +0200

    test: Add tests for coverage

--- tests/ArrayOrganizeFacadeTest.php
+++ tests/ArrayOrganizeFacadeTest.php
@@ -29,6 +29,48 @@ class ArrayOrganizeFacadeTest extends TestCase
             ["id" => 2, "name" => "test4"],
             ["id" => 1, "name" => "test5"]
         ], $result);
+
+        $result = ArrayOrganizeFacade::dataSort($data, 'id', 'ASC');
+
+        $this->assertEquals([
+            ["id" => 1, "name" => "test5"],
+            ["id" => 2, "name" => "test4"],
+            ["id" => 3, "name" => "test3"]
+        ], $result);
+
+        $this->assertEquals(false, ArrayOrganizeFacade::dataSort([], 'id', 'ASC'));
+    }
+
+    /**
+     * DataSort function invalid argument test
+     * @covers ::dataSort
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testDataSortInvalidArgument(): void
+    {
+        $data = [
+            ["id" => 2, "name" => "test4"],
+            ["id" => 1, "name" => "test5"],
+            ["id" => 3, "name" => "test3"]
+        ];
+        $this->expectException(\InvalidArgumentException::class);
+        ArrayOrganizeFacade::dataSort($data, 'text', 'DESC');
+    }
+
+    /**
+     * DataSort function invalid data test
+     * @covers ::dataSort
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testDataSortInvalidData(): void
+    {
+        $data = [
+            0 => "test4",
+            1 => "test5",
+            2 => "test3"
+        ];
+        $this->expectException(\Exception::class);
+        ArrayOrganizeFacade::dataSort($data, 'id', 'DESC');
     }
 
     /**
@@ -50,6 +92,46 @@ class ArrayOrganizeFacadeTest extends TestCase
             ["name" => "test4"],
             ["name" => "test5"]
         ], $result);
+
+        $result = ArrayOrganizeFacade::dataColumnFilter($data, "keep", ["name"]);
+        $this->assertEquals([
+            ["name" => "test3"],
+            ["name" => "test4"],
+            ["name" => "test5"]
+        ], $result);
+
+        $result = ArrayOrganizeFacade::dataColumnFilter([], "skip", ["id"]);
+        $this->assertEquals(false, $result);
+    }
+
+    /**
+     * DataColumnFilter function invalid argument test
+     * @covers ::dataColumnFilter
+     * @uses \SimonDevelop\ArrayOrganizeFacade
+     */
+    public function testDataColumnFilterInvalidArgument(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        ArrayOrganizeFacade::dataColumnFilter([
+            ["id" => 2, "name" => "test3"],
+            ["id" => 1, "name" => "test4"],
+            ["id" => 3, "name" => "test5"]
+        ], "test", ["id"]);
+    }
+
+    /**
+     * dataColumnFilter function invalid data test
+     * @covers ::dataColumnFilter
+     * @uses \SimonDevelop\ArrayOrganizeFacade
+     */
+    public function testDataColumnFilterInvalidData(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        ArrayOrganizeFacade::dataColumnFilter([
+            0 => "test4",
+            1 => "test5",
+            2 => "test3"
+        ], "skip", ["id"]);
     }
 
     /**
@@ -144,9 +226,84 @@ class ArrayOrganizeFacadeTest extends TestCase
             ["id" => 1, "name" => "php is a live"],
         ], $result);
 
+        // date lower
+        $data = [
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ];
+        $result = ArrayOrganizeFacade::dataFilter($data, ["date[<]" => "2021-07-24"]);
+        $this->assertEquals([
+            ["id" => 2, "date" => "2021-08-21"]
+        ], $result);
+
+        // date lower and equal
+        $data = [
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ];
+        $result = ArrayOrganizeFacade::dataFilter($data, ["date[<=]" => "2021-07-24"]);
+        $this->assertEquals([
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ], $result);
+
+        // date upper
+        $data = [
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ];
+        $result = ArrayOrganizeFacade::dataFilter($data, ["date[>]" => "2021-07-24"]);
+        $this->assertEquals([
+            ["id" => 1, "date" => "2021-07-21"]
+        ], $result);
+
+        // date upper and equal
+        $data = [
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ];
+        $result = ArrayOrganizeFacade::dataFilter($data, ["date[>=]" => "2021-07-24"]);
+        $this->assertEquals([
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ], $result);
+        
         // error
         $result = ArrayOrganizeFacade::dataFilter([], ["id" => 2]);
-
         $this->assertEquals(false, $result);
+    }
+
+    /**
+     * DataFilter function invalid data one test
+     * @covers ::dataFilter
+     * @uses \SimonDevelop\ArrayOrganizeFacade
+     */
+    public function testDataFilterInvalidDataOne(): void
+    {
+        $this->expectException(\Exception::class);
+        ArrayOrganizeFacade::dataFilter([
+            0 => "test3",
+            1 => "test4",
+            2 => "test5"
+        ], ["id" => 2]);
+    }
+
+    /**
+     * DataFilter function invalid data two test
+     * @covers ::dataFilter
+     * @uses \SimonDevelop\ArrayOrganizeFacade
+     */
+    public function testDataFilterInvalidDataTwo(): void
+    {
+        $this->expectException(\Exception::class);
+        ArrayOrganizeFacade::dataFilter([
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ], ["id" => []]);
     }
 }
--- tests/ArrayOrganizeTest.php
+++ tests/ArrayOrganizeTest.php
@@ -43,6 +43,26 @@ class ArrayOrganizeTest extends TestCase
     }
 
     /**
+     * Constructor test invalid argument
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testInitConstructorInvalidArgumentOne(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new ArrayOrganize([], 0, 2);
+    }
+
+    /**
+     * Constructor test invalid argument
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testInitConstructorInvalidArgumentTwo(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new ArrayOrganize([], 10, 0);
+    }
+
+    /**
      * DataSort function test
      * @covers ::dataSort
      * @covers ::getData
@@ -64,11 +84,57 @@ class ArrayOrganizeTest extends TestCase
             ["id" => 1, "name" => "test5"]
         ], $ArrayOrganize->getData());
 
+        $ArrayOrganize = new ArrayOrganize([
+            ["id" => 2, "name" => "test4"],
+            ["id" => 1, "name" => "test5"],
+            ["id" => 3, "name" => "test3"]
+        ]);
+        $result = $ArrayOrganize->dataSort('id', 'ASC');
+
+        $this->assertEquals(true, $result);
+        $this->assertEquals([
+            ["id" => 1, "name" => "test5"],
+            ["id" => 2, "name" => "test4"],
+            ["id" => 3, "name" => "test3"]
+        ], $ArrayOrganize->getData());
+
         $ArrayOrganize = new ArrayOrganize();
         $result = $ArrayOrganize->dataSort('id', 'DESC');
 
         $this->assertEquals(false, $result);
         $this->assertEquals([], $ArrayOrganize->getData());
+    }
+
+    /**
+     * DataSort function invalid argument test
+     * @covers ::dataSort
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testDataSortInvalidArgument(): void
+    {
+        $ArrayOrganize = new ArrayOrganize([
+            ["id" => 2, "name" => "test4"],
+            ["id" => 1, "name" => "test5"],
+            ["id" => 3, "name" => "test3"]
+        ]);
+        $this->expectException(\InvalidArgumentException::class);
+        $ArrayOrganize->dataSort('text', 'DESC');
+    }
+
+    /**
+     * DataSort function invalid data test
+     * @covers ::dataSort
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testDataSortInvalidData(): void
+    {
+        $ArrayOrganize = new ArrayOrganize([
+            0 => "test4",
+            1 => "test5",
+            2 => "test3"
+        ]);
+        $this->expectException(\Exception::class);
+        $ArrayOrganize->dataSort('id', 'DESC');
     }
 
     /**
@@ -98,6 +164,52 @@ class ArrayOrganizeTest extends TestCase
 
         $this->assertEquals(false, $result);
         $this->assertEquals([], $ArrayOrganize->getData());
+
+        $ArrayOrganize = new ArrayOrganize([
+            ["id" => 2, "name" => "test3"],
+            ["id" => 1, "name" => "test4"],
+            ["id" => 3, "name" => "test5"]
+        ]);
+        $result = $ArrayOrganize->dataColumnFilter("keep", ["id"]);
+
+        $this->assertEquals(true, $result);
+        $this->assertEquals([
+            ["id" => 2,],
+            ["id" => 1,],
+            ["id" => 3,]
+        ], $ArrayOrganize->getData());
+    }
+
+    /**
+     * DataColumnFilter function invalid argument test
+     * @covers ::dataColumnFilter
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testDataColumnFilterInvalidArgument(): void
+    {
+        $ArrayOrganize = new ArrayOrganize([
+            ["id" => 2, "name" => "test3"],
+            ["id" => 1, "name" => "test4"],
+            ["id" => 3, "name" => "test5"]
+        ]);
+        $this->expectException(\InvalidArgumentException::class);
+        $ArrayOrganize->dataColumnFilter("test", ["id"]);
+    }
+
+    /**
+     * dataColumnFilter function invalid data test
+     * @covers ::dataColumnFilter
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testDataColumnFilterInvalidData(): void
+    {
+        $ArrayOrganize = new ArrayOrganize([
+            0 => "test4",
+            1 => "test5",
+            2 => "test3"
+        ]);
+        $this->expectException(\InvalidArgumentException::class);
+        $ArrayOrganize->dataColumnFilter("skip", ["id"]);
     }
 
     /**
@@ -200,12 +312,94 @@ class ArrayOrganizeTest extends TestCase
             ["id" => 1, "name" => "php is a live"],
         ], $ArrayOrganize->getData());
 
+        // date lower
+        $ArrayOrganize = new ArrayOrganize([
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ]);
+        $result = $ArrayOrganize->dataFilter(["date[<]" => "2021-07-24"]);
+        $this->assertEquals(true, $result);
+        $this->assertEquals([
+            ["id" => 2, "date" => "2021-08-21"]
+        ], $ArrayOrganize->getData());
+
+        // date lower and equal
+        $ArrayOrganize = new ArrayOrganize([
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ]);
+        $result = $ArrayOrganize->dataFilter(["date[<=]" => "2021-07-24"]);
+        $this->assertEquals(true, $result);
+        $this->assertEquals([
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ], $ArrayOrganize->getData());
+
+        // date upper
+        $ArrayOrganize = new ArrayOrganize([
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ]);
+        $result = $ArrayOrganize->dataFilter(["date[>]" => "2021-07-24"]);
+        $this->assertEquals(true, $result);
+        $this->assertEquals([
+            ["id" => 1, "date" => "2021-07-21"]
+        ], $ArrayOrganize->getData());
+
+        // date upper and equal
+        $ArrayOrganize = new ArrayOrganize([
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ]);
+        $result = $ArrayOrganize->dataFilter(["date[>=]" => "2021-07-24"]);
+        $this->assertEquals(true, $result);
+        $this->assertEquals([
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ], $ArrayOrganize->getData());
+        
         // error
         $ArrayOrganize = new ArrayOrganize();
         $result = $ArrayOrganize->dataFilter(["id" => 2]);
 
         $this->assertEquals(false, $result);
         $this->assertEquals([], $ArrayOrganize->getData());
+    }
+
+    /**
+     * DataFilter function invalid data one test
+     * @covers ::dataFilter
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testDataFilterInvalidDataOne(): void
+    {
+        $ArrayOrganize = new ArrayOrganize([
+            0 => "test3",
+            1 => "test4",
+            2 => "test5"
+        ]);
+        $this->expectException(\Exception::class);
+        $ArrayOrganize->dataFilter(["id" => 2]);
+    }
+
+    /**
+     * DataFilter function invalid data two test
+     * @covers ::dataFilter
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testDataFilterInvalidDataTwo(): void
+    {
+        $ArrayOrganize = new ArrayOrganize([
+            ["id" => 2, "date" => "2021-08-21"],
+            ["id" => 1, "date" => "2021-07-21"],
+            ["id" => 3, "date" => "2021-07-24"]
+        ]);
+        $this->expectException(\Exception::class);
+        $ArrayOrganize->dataFilter(["id" => []]);
     }
 
     /**
@@ -271,6 +465,49 @@ class ArrayOrganizeTest extends TestCase
             ["id" => 1, "name" => []],
         ], 3, 1);
         $this->assertEquals(false, $ArrayOrganize->generateTable($css, $pager));
+
+        $ArrayOrganize = new ArrayOrganize([], 3, 1);
+        $this->assertEquals(false, $ArrayOrganize->generateTable([], []));
+
+        $ArrayOrganize = new ArrayOrganize($datas, 3, 1);
+        $pager = [
+            "position" => "top",
+            "lang" => [
+                "previous" => "<< Previous",
+                "next" => "Next >>"
+            ],
+            "cssClass" => [
+                "ul" => "pagination",
+                "li" => "page-item",
+                "a" => "page-link",
+                "disabled" => [
+                    "li" => "disabled"
+                ],
+                "active" => [
+                    "li" => "active"
+                ]
+            ],
+            "url" => "index.php?p={}"
+        ];
+
+        $html = $ArrayOrganize->generateTable([], $pager);
+        $this->assertStringContainsString('<table class="">', $html);
+
+        $this->assertStringContainsString('<tr><th>id</th><th>name</th></tr>', $html);
+        $this->assertStringContainsString('<tr><td>2</td><td>example 5</td></tr>', $html);
+        $this->assertStringContainsString('<tr><td>1</td><td>example 5</td></tr>', $html);
+        $this->assertStringContainsString('<tr><td>3</td><td>example 3</td></tr>', $html);
+
+        $this->assertStringContainsString('<ul class="pagination">', $html);
+        $this->assertStringContainsString('<li class="page-item disabled">', $html);
+        $this->assertStringContainsString('<a class="page-link"><< Previous</a>', $html);
+        $this->assertStringContainsString('<li class="page-item active">', $html);
+        $this->assertStringContainsString('<a class="page-link">1</a>', $html);
+        $this->assertStringContainsString('<li class="page-item">', $html);
+        $this->assertStringContainsString(
+            '<a class="page-link" href="index.php?p=2">2</a>',
+            $html
+        );
     }
 
     /**
@@ -289,7 +526,7 @@ class ArrayOrganizeTest extends TestCase
             ["id" => 4, "name" => "example 6"]
         ];
 
-        $ArrayOrganize = new ArrayOrganize($datas, 3, 1);
+        $ArrayOrganize = new ArrayOrganize($datas, 4, 1);
 
         $pager = [
             "position" => "bottom",
@@ -327,6 +564,86 @@ class ArrayOrganizeTest extends TestCase
             '<a class="page-link" href="#">2</a>',
             $html
         );
+
+        $pager = [
+            "position" => "bottom",
+            "lang" => [
+                "previous" => "<< Previous",
+                "next" => "Next >>"
+            ],
+            "cssClass" => [
+                "disabled" => [
+                    "a" => "disabled"
+                ],
+                "active" => [
+                    "a" => "active"
+                ]
+            ],
+            "url" => "index.php?p={}"
+        ];
+
+        $html = $ArrayOrganize->generatePagination($pager);
+
+        $this->assertStringContainsString('<ul class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<a class="disabled"><< Previous</a>', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<a class="active">1</a>', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+
+        $pager = [
+            "position" => "bottom",
+            "lang" => [
+                "previous" => "<< Previous",
+                "next" => "Next >>"
+            ],
+            "cssClass" => [],
+            "url" => "index.php?p={}"
+        ];
+
+        $html = $ArrayOrganize->generatePagination($pager);
+
+        $this->assertStringContainsString('<ul class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+
+        $pager = [
+            "position" => "bottom",
+            "lang" => [
+                "previous" => "<< Previous",
+                "next" => "Next >>"
+            ],
+            "url" => "index.php?p={}"
+        ];
+
+        $html = $ArrayOrganize->generatePagination($pager);
+
+        $this->assertStringContainsString('<ul class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+
+        $pager = [
+            "position" => "bottom",
+        ];
+
+        $html = $ArrayOrganize->generatePagination($pager);
+
+        $this->assertStringContainsString('<ul class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+
+        $ArrayOrganize->setByPage(3);
+        $ArrayOrganize->setPage(2);
+        $ArrayOrganize->setUrl("index.php?p={}");
+        $html = $ArrayOrganize->generatePagination($pager);
+
+        $this->assertStringContainsString('<ul class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
+        $this->assertStringContainsString('<li class="">', $html);
     }
 
     /**
@@ -368,6 +685,23 @@ class ArrayOrganizeTest extends TestCase
     }
 
     /**
+     * AddFunction function invalid data test
+     * @covers ::addFunction
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testAddFunctionInvalidData(): void
+    {
+        $datas = [
+            2 => "example 5",
+            1 => "example 5",
+            3 => "example 3"
+        ];
+        $ArrayOrganize = new ArrayOrganize($datas);
+        $this->expectException(\Exception::class);
+        $ArrayOrganize->addFunction("name", "substr", [0, -2]);
+    }
+
+    /**
      * AddTotal function test
      * @covers ::addTotal
      * @uses \SimonDevelop\ArrayOrganize
@@ -395,6 +729,41 @@ class ArrayOrganizeTest extends TestCase
             '<tr><td></td><td><strong>Total :</strong> 54</td></tr></tbody>',
             $ArrayOrganize->generateTable()
         );
+    }
+
+    /**
+     * AddTotal function invalid data test
+     * @covers ::addTotal
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testAddTotalInvalidData(): void
+    {
+        $datas = [
+            2 => 6,
+            1 => 6,
+            3 => 3
+        ];
+        $ArrayOrganize = new ArrayOrganize($datas);
+        $this->expectException(\Exception::class);
+        $ArrayOrganize->addTotal("val", "<strong>Total :</strong> ");
+    }
+
+    /**
+     * AddTotal function invalid filtre test
+     * @covers ::addTotal
+     * @uses \SimonDevelop\ArrayOrganize
+     */
+    public function testAddTotalInvalidFiltre(): void
+    {
+        $datas = [
+            ["id" => 2, "val" => 6],
+            ["id" => 1, "val" => 6],
+            ["id" => 3, "val" => 3],
+            ["id" => 6, "val" => 5]
+        ];
+        $ArrayOrganize = new ArrayOrganize($datas);
+        $result = $ArrayOrganize->addTotal("text", "<strong>Total :</strong> ");
+        $this->assertEquals(false, $result);
     }
 
     /**
@@ -449,6 +818,76 @@ class ArrayOrganizeTest extends TestCase
             ["test" => "title 2"],
         ]);
         $this->assertEquals(false, $ArrayOrganize->generateList($cssClass));
+
+        $ArrayOrganize = new ArrayOrganize($datas);
+
+        $cssClass = [
+            "a" => "list-link",
+            "disabled" => [
+                "a" => "disabled",
+            ],
+            "active" => [
+                "a" => "active",
+            ],
+            "balise" => "a/li"
+        ];
+
+        $html = $ArrayOrganize->generateList($cssClass);
+        $this->assertStringContainsString('<ul class="">', $html);
+        $this->assertStringContainsString('<a class="list-link">', $html);
+        $this->assertStringContainsString('<li class="">Test list 1</li>', $html);
+        $this->assertStringContainsString(
+            '<a class="list-link" href="http://ddg.gg/" target="_blank"><li class="">Test list 2</li></a>',
+            $html
+        );
+
+        $ArrayOrganize = new ArrayOrganize($datas);
+
+        $cssClass = [
+            "li" => "list-item",
+            "balise" => "li"
+        ];
+
+        $html = $ArrayOrganize->generateList($cssClass);
+        $this->assertStringContainsString('<ul class="">', $html);
+        $this->assertStringContainsString('<li class="list-item">Test list 1</li>', $html);
+
+        $ArrayOrganize = new ArrayOrganize($datas);
+
+        $cssClass = [
+            "a" => "list-link",
+            "balise" => "a"
+        ];
+
+        $html = $ArrayOrganize->generateList($cssClass);
+        $this->assertStringContainsString('<ul class="">', $html);
+        $this->assertStringContainsString('<a class="list-link" >Test list 1</a>', $html);
+        $this->assertStringContainsString(
+            '<a class="list-link"  href="http://ddg.gg/" target="_blank">Test list 2</a>',
+            $html
+        );
+
+        $datas = [
+            ["title" => "Test list 1"],
+            [
+                "title" => "Test list 2",
+                "url" => "http://ddg.gg/",
+                "target_blank" => true
+            ],
+            [
+                "title" => "Test list 3",
+                "active" => true
+            ],
+            [
+                "title" => "Test list 4",
+                "disabled" => true
+            ]
+        ];
+
+        $ArrayOrganize = new ArrayOrganize($datas);
+
+        $html = $ArrayOrganize->generateList([]);
+        $this->assertStringContainsString('<ul class=""></ul>', $html);
     }
 
     /**
@@ -479,5 +918,38 @@ class ArrayOrganizeTest extends TestCase
         $this->assertEquals(2, $page);
         $this->assertEquals("index.php?=2", $url);
         $this->assertEquals(["test1", "test2"], $data);
+    }
+
+    /**
+     * setByPage test invalid argument
+     * @depends testInitConstructor
+     * @covers ::setByPage
+     */
+    public function testSetByPage($ArrayOrganize): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $ArrayOrganize->setByPage(0);
+    }
+
+    /**
+     * setPage test invalid argument
+     * @depends testInitConstructor
+     * @covers ::setPage
+     */
+    public function testSetPage($ArrayOrganize): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $ArrayOrganize->setPage(0);
+    }
+
+    /**
+     * setUrl test invalid argument
+     * @depends testInitConstructor
+     * @covers ::setUrl
+     */
+    public function testSetUrl($ArrayOrganize): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $ArrayOrganize->setUrl('');
     }
 }
